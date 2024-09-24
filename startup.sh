bf01eea2063cb9a74eca5764ca6ae2a01e238e0a
Maider Ibarra <maider.ibarra@gmail.com>
2024-09-24 19:49:04 +0200

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Crear directorios necesarios
+mkdir -p /var/run/sshd
+chown -R root:root /root
+
+# Iniciar Xvfb para crear un entorno gráfico
+Xvfb :0 -screen 0 1280x720x24 &
+sleep 2  # Espera un momento para asegurarte de que Xvfb esté listo
+
+# Establecer la variable de entorno DISPLAY
+export DISPLAY=:0
+
+# Iniciar el entorno de escritorio MATE
+mate-session &
+
+# Iniciar x11vnc sin contraseña
+x11vnc -display :0 -forever -nopw -noxdamage -repeat &
+sleep 2  # Espera un momento para asegurarte de que x11vnc esté listo
+
+# Iniciar noVNC
+websockify --web /usr/share/novnc/ 6080 localhost:5900 &
+
+# Esperar indefinidamente
+wait
